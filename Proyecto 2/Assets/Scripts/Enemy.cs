40ab251433545cf18070111cd90996dfd8f61022
Kevin Barrantes Cerdas <kabarrantes163@gmail.com>
2020-11-19 16:31:32 -0600

    Bot spawner created
    Created a game object called BotController that instantiates the initial population of bots in a specified position. Each bot has basic stats, health, speed and explosion radius that are randomized upon creation. Bots have no functionality as of now.

---
+++ Proyecto 2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D _enemyBody;
+
+    public int health;
+    public int speed;
+    public int radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    private void OnEnable()
+    {
+        _enemyBody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Spawn(float xPosition)
+    {
+        
+        _enemyBody.position = new Vector2(xPosition,0);
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Proyecto 2/Assets/Scripts/GeneticController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class GeneticController : MonoBehaviour
+{
+    private GameObject[] _pool;
+    private int _skillPoints = 10;
+    [SerializeField] private GameObject bot;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        _pool = new GameObject[8];
+        float xPosition = 0;
+        for (int botCount = 0; botCount < 8; botCount++)
+        {
+            _pool[botCount] = Instantiate(bot);
+            _pool[botCount].GetComponent<Enemy>().Spawn(xPosition);
+            xPosition += 1;
+        }
+        
+        RandomizeStarters();
+        
+
+    }
+
+    private void RandomizeStarters()
+    {
+        var upperLimit = 4;
+        Random randRange = new Random();
+
+        for (int botCount = 0; botCount < 8; botCount++)
+        {
+            Enemy currentBot = _pool[botCount].GetComponent<Enemy>();
+            while (_skillPoints > 0)
+            {
+                var num = 0;
+                num = randRange.Next(1, upperLimit);
+                switch (num)
+                {
+                    case 1:
+                        currentBot.health++;
+                        _skillPoints--;
+                        break;
+                    case 2:
+                        currentBot.speed++;
+                        _skillPoints--;
+                        break;
+                    case 3:
+                        currentBot.radius++;
+                        _skillPoints--;
+                        break;
+                }
+            }
+
+            _skillPoints = 10;
+            
+            if (currentBot.health == 0)
+            {
+                currentBot.health+=2;
+                currentBot.radius--;
+                currentBot.speed--;
+            }else if (currentBot.speed == 0)
+            {
+                currentBot.health--;
+                currentBot.radius--;
+                currentBot.speed+=2;
+                
+            }else if (currentBot.radius == 0)
+            {
+                currentBot.health--;
+                currentBot.radius+=2;
+                currentBot.speed--;
+            }
+        }
+        
+    }
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
