d1f62d4be95784a62b654979a6903c0e188541c9
Kevin Barrantes Cerdas <kabarrantes163@gmail.com>
2020-11-21 17:08:57 -0600

    Bombs now destroy blocks, Bombs damage player
    Implemented a function that deducts health points from the player when he is in the radius of a bomb explosion. Destroyable walls are now recognized by the bomb object, prompting deletion upon the explosion.

--- Proyecto 2/Assets/Scripts/BombBehaviour.cs
+++ Proyecto 2/Assets/Scripts/BombBehaviour.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 
 public class BombBehaviour : MonoBehaviour
 {
-    public float timer = 15f;
+    public float timer = 3f;
     public float radius = 6f;
+    private BoxCollider2D _horizontal;
+    private BoxCollider2D _vertical;
+    
+    
 
     
     // Start is called before the first frame update
@@ -16,19 +20,50 @@ public class BombBehaviour : MonoBehaviour
         
     }
 
-    
-
-    void Explode()
+    private void OnEnable()
     {
+        BoxCollider2D[] list = GetComponents<BoxCollider2D>();
+        _vertical = list[0];
+        _horizontal = list[1];
+        
+        _vertical.size = new Vector2(2.5f,14);
+        _horizontal.size = new Vector2(radius,2.5f);
+    }
+
+    public void ChangeExplosionRadius()
+    {
+        _vertical.size = new Vector2(1,radius);
+        _horizontal.size = new Vector2(radius,1);
+    }
+
+
+    public void Explode()
+    {
+
         timer -= Time.deltaTime;
-        if (timer < 0)
+        if (timer <= 0)
         {
             Debug.Log("BOOOOM");
+            
             Destroy(gameObject);
         }
     }
 
-    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+        
+        if (other.CompareTag("Destructible") && timer<=0.05)
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Player")&&timer<=0.05)
+        {
+            other.GetComponent<CharacterManager>().ReduceHealth();
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
--- Proyecto 2/Assets/Scripts/CharacterManager.cs
+++ Proyecto 2/Assets/Scripts/CharacterManager.cs
@@ -10,12 +10,15 @@ using Vector2 = UnityEngine.Vector2;
 
 public class CharacterManager : MonoBehaviour
 {
-    public int health;
+    public int health = 5;
     public float speed = 3f;
     private Rigidbody2D _playerBody;
     public PlayerActions playerInput;
     [SerializeField] private GameObject bomb;
     private Vector2 _lookDirection = new Vector2(1,0);
+    private bool _isInvincible = false;
+    private float _invincibleTime = 2f;
+    private float _invincibleTimer;
 
     private void Awake()
     {
@@ -35,6 +38,7 @@ public class CharacterManager : MonoBehaviour
     void Start()
     {
         _playerBody = GetComponent<Rigidbody2D>();
+        
     }
     
     private void MoveOnPerformed(InputAction.CallbackContext context)
@@ -59,24 +63,28 @@ public class CharacterManager : MonoBehaviour
         {
             bombPosition.Set(position.x-0.03f,position.y);
             GameObject bombObject = Instantiate(bomb,bombPosition, Quaternion.identity);
+            
         }
 
         if (_lookDirection.x > 0f)
         {
             bombPosition.Set(position.x+0.03f,position.y);
             GameObject bombObject = Instantiate(bomb,bombPosition, Quaternion.identity);
+            
         }
 
         if (_lookDirection.y < 0f)
         {
             bombPosition.Set(position.x,position.y-0.8f);
             GameObject bombObject = Instantiate(bomb,bombPosition, Quaternion.identity);
+            
         }
         
         if (_lookDirection.y > 0f)
         {
             bombPosition.Set(position.x,position.y+0.8f);
             GameObject bombObject = Instantiate(bomb,bombPosition, Quaternion.identity);
+            
         }
 
     }
@@ -98,9 +106,40 @@ public class CharacterManager : MonoBehaviour
 
     }
 
+    public void ReduceHealth()
+    {
+        if (_isInvincible)
+        {
+            return;
+        }
+        
+        health -= 1;
+        _isInvincible = true;
+        _invincibleTimer = _invincibleTime;
+        Debug.Log(health);
+    }
+
+    private void InvincibleTimeManager()
+    {
+        if (_isInvincible)
+        {
+            _invincibleTimer -= Time.deltaTime;
+            if (_invincibleTimer < 0)
+            {
+                _isInvincible = false;
+            }
+        }
+    }
+
+    private void CheckDeath()
+    {
+        if (health == 0) Destroy(gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        InvincibleTimeManager();
+        CheckDeath();
     }
 }
